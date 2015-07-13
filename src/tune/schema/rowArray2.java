7f884e11852d6e0a5d3cce0782c176b409e4348c
fuat <fuat@localhost.localdomain>
2015-07-13 15:32:13 -0700

    changes

--- src/tune/schema/rowArray2.java
+++ src/tune/schema/rowArray2.java
@@ -1,165 +1,70 @@
 package tune.schema;
 
-import google.flatbuffers.Constants;
-
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 import java.io.*;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.jets3t.service.io.GZipInflatingOutputStream;
-
 import tune.schema.rawLog;
 
-//import com.google.flatbuffers.FlatBufferBuilder;
-
+/**
+ * 
+ * @author fuat
+ *
+ */
 public class rowArray2 {
 
-	static int initial_capacity;
-	static int position;
-	static int anotherCapacity;
-	static int arrayLength;
-	static int lengthTracker = 0;
-	static int FILE_64_MBtoBytes = 64 * 1028; 
-	public static void main(String[] args) throws FileNotFoundException, IOException{
-            byte[] data = null;
-        File file = new File("/home/fuat/2015/03/21/shard14_20150321_rev0_prison00_batcher00_seq0.fb.gz");
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
+		File file = new File(
+				"/home/fuat/2015/03/21/shard14_20150321_rev0_prison00_batcher00_seq0.fb.gz");
 
+		@SuppressWarnings("resource")
+		InputStream stream = new GZIPInputStream(new FileInputStream(file));
 
-               // byte[] data = null;
-                //InputStream file =  new InputStream( new BufferReader(new File("/home/fuat/2015/03/21/shard14_20150321_rev0_prison00_batcher00_seq0.fb.gz")));
-               // DataInputStream stream = new DataInputStream(
-                	  //new BufferedInputStream(new FileInputStream(new File("rawLog"))));
-                
-               
-                
-                
-               InputStream stream =  new GZIPInputStream(new FileInputStream(file), 1024*1024);
-                
-               //DataInputStream stream1 = (DataInputStream) stream;
-                //data = IOUtils.toByteArray(new GZIPInputStream( new FSDataInputStream("/home/fuat/2015/03/21/shard14_20150321_rev0_prison00_batcher00_seq0.fb.gz")));
-                //will change this loop later based on existing of stream.
-                int bytes = FILE_64_MBtoBytes;
-               
-            for(int i = 0; i < 8; i++){   
-               byte[] bs = new byte[4];
-              // int offset3 = stream.readInt();
-              // int c;
-               stream.read(bs, 0, 4);
-               
-               ByteBuffer bff2 = ByteBuffer.wrap(bs);
-              // System.out.println(bff2.getInt());
-               //int i = stream.read(bs);
-               //System.out.println(data1+ "  " +i);
-               bff2.clear();
-               int position1 = bff2.getInt() ;
-               //int bufferSize = java.nio.ByteBuffer.wrap(bs).getShort();
-              // System.out.println(bufferSize);
-               System.out.println(position1);
-               
-               byte[] bs2 = new byte[position1];
-               
-               stream.read(bs2,0, position1 );
-               
-               ByteBuffer bff3 = ByteBuffer.wrap(bs2);
-               
-               printData(bs2);
-               
-              // bytes = bytes - bs.length - bs2.length;
-            }   
-              /* 
-               for(byte b:bs)
-               {
-                  // converts byte to int
-                  c=(int)b;
-                  if(b==0)
-                     c='-';
-                  
-                  // print
-                  System.out.println(c);
-               } */
-               
-               //System.out.println(data1);
-             //  System.out.println(stream.read());
-              // byte[] readBuffer3 = Arrays.copyOf(stream, 4, offset3+4);
-                //System.out.println(50331716/8);
-              //  System.out.println(stream.readInt());
-               // stream.read(stream, 0, 4);
-                
-             //   data= IOUtils.toByteArray(stream);
-                
-                //data = ByteStreams.toByteArray(file);
-              /* System.out.println(data);
+		int i = 0;
 
-                ByteBuffer bb = ByteBuffer.wrap(data);
+		while (true) {
 
-                while (bb.hasRemaining()) {
-                	int size = bb.getInt(); 
-                	byte[] record = new byte[size];
-                	bb.get(record);
-                    printData(record);
-                   
+			if (stream.available() <= 0)
+				break;
+			i++;
+			byte[] bs = new byte[4];
 
-                }*/
-                
-   
-                
-    
-             
-             //  arrayLength = data.length;
-               //System.out.println(arrayLength);
-               
-               //System.out.println(data.getClass().getName());
-               //System.out.println(data[3]);
-               /*byte[] offset = Arrays.copyOfRange(data, 0, 4);
-               ByteBuffer bff = ByteBuffer.wrap(offset);
-               int offset1 = bff.getInt();
-               System.out.println(offset1);
-               
-               System.out.println(offset.length);
-               byte[] readBuffer = Arrays.copyOfRange(data, 4, offset1);
-               //ByteBuffer bff2 = ByteBuffer.wrap(readBuffer);
-               printData(readBuffer);
-               */
-             /* int ll = data.length;
-               while(ll> 0){
-            	   byte[] offset = Arrays.copyOfRange(data, 0, 4);
-            	   ByteBuffer bff2 = ByteBuffer.wrap(offset);
-            	   int bufferLength = bff2.getInt();
-            	   ll = data.length;
-            	   try{
-            	   byte[] readBuffer2 = Arrays.copyOfRange(data, 4, bufferLength);
-            	   printData(readBuffer2);
-            	   lengthTracker = 4+bufferLength;
-            	   //System.out.println(bufferLength);
-            	   //System.out.println(lengthTracker);
-            	   data = Arrays.copyOfRange(data, 4+bufferLength, arrayLength);
-            	   
-            	   
-            	   }catch(IllegalArgumentException e) {
-            		   
-            	   }
-               }*/
-            
+			Integer readBytes = 0;
+			while (4 - readBytes > 0) {
+				readBytes += stream.read(bs, readBytes, 4 - readBytes);
+			}
 
-             
-           	
-          		
-           	
+			ByteBuffer bff2 = ByteBuffer.wrap(bs);
+			int position1 = bff2.getInt();
+			System.out.println(position1);
 
-}
+			byte[] bs2 = new byte[position1];
 
+			Integer bytesRead = 0;
+			while (position1 - bytesRead > 0) {
+				bytesRead += stream.read(bs2, bytesRead, position1 - bytesRead);
+			}
+
+			printData(bs2);
+
+		}
+		System.out.println("row no:" + (i + 1));
+
+	}
+
+	/**
+	 * 
+	 * @param record
+	 */
 	private static void printData(byte[] record) {
-		
+
 		ByteBuffer bb = ByteBuffer.wrap(record);
-		//if(bb.hasRemaining()){
+
 		rawLog rlog = tune.schema.rawLog.getRootAsrawLog(bb);
 		System.out.println(rlog.advertiserId() + "  " + rlog.created() + " "
 				+ rlog.clickCreated() + "  " + rlog.advertiserSubAdgroup()
 				+ " " + rlog.attributedIdDate());
-		//}
-		bb.clear();
+
 	}
 }
