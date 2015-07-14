f1a68123e9d934ad5b5ad751c5077beeeef2feff
fuat <fuat@localhost.localdomain>
2015-07-14 09:37:45 -0700

    changes on record reader interface

--- src/tune/schema/rowArrayV3.java
+++ src/tune/schema/rowArrayV3.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.ql.exec.RecordReader;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.LineRecordReader.LineReader;
+import org.apache.hadoop.io.Text;
 
 import tune.schema.rawLog;
 
@@ -18,11 +20,14 @@ import tune.schema.rawLog;
  * @author fuat
  *
  */
+@SuppressWarnings("deprecation")
 public class rowArrayV3 implements RecordReader{
 
 	private InputStream fileInputStream;
 	private Configuration conf;
 	private Properties pro;
+	private LineReader lineReader;
+	private Text row;
 	
 	/*public static void main(String[] args) throws FileNotFoundException,
 			IOException {
@@ -78,6 +83,8 @@ public class rowArrayV3 implements RecordReader{
 
 	public Writable createRow() throws IOException {
 		
+		
+		
 		if (fileInputStream.available() <= 0)
 			System.exit(0);
 		
@@ -98,23 +105,38 @@ public class rowArrayV3 implements RecordReader{
 		while (position1 - bytesRead > 0) {
 			bytesRead += fileInputStream.read(bs2, bytesRead, position1 - bytesRead);
 		}
+		
+		
+		
+		
+		ByteBuffer bb = ByteBuffer.wrap(bs2);
 
+		rawLog rlog = tune.schema.rawLog.getRootAsrawLog(bb);
+		String s  = rlog.advertiserId() + "  " + rlog.created() + " "
+				+ rlog.clickCreated() + "  " + rlog.advertiserSubAdgroup()
+				+ " " + rlog.attributedIdDate();
 
-	return  new BytesWritable(bs2);
+		row = new Text(s);
+	return  row;
 		
 	}
 
 	public void initialize(InputStream fileInputStream, Configuration conf, Properties pro)
 			throws IOException {
-		this.fileInputStream = fileInputStream;
+		//this.fileInputStream = fileInputStream;
+		lineReader = new LineReader(fileInputStream, conf);
 		this.conf = conf;
 		this.pro = pro;
 		
 	}
 
-	public int next(Writable arg0) throws IOException {
+	public int next(Writable row) throws IOException {
 		
-		return 0;
+
+		if(lineReader == null)
+			return -1;
+		
+		return lineReader.readLine((Text) row);
 	}
 }
 
